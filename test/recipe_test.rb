fea885d559a580a506be3ffa1bff61260476f5b4
Norma Lopez <norma_l304@yahoo.com>
2020-06-03 11:00:24 -0600

    Add #total_calories with passing test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/recipe'
+require 'pry'
 
 class RecipeTest < Minitest::Test
   def test_it_exists
@@ -39,5 +40,22 @@ class RecipeTest < Minitest::Test
     recipe1.add_ingredient(ingredient2, 8)
 
     assert_equal [ingredient1, ingredient2], recipe1.ingredients
-  end 
+  end
+
+  def test_it_has_total_calories
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    assert_equal 440, recipe1.total_calories
+    assert_equal 675, recipe2.total_calories
+  end
 end
