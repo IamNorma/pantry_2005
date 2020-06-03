017b0c506a2a2829940f7b951eaf32fb97834227
Norma Lopez <norma_l304@yahoo.com>
2020-06-03 12:10:19 -0600

    Add #enough_ingredients_for? test not passing

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -10,6 +10,16 @@ class Pantry
   end
 
   def restock(ingredient, amount)
-    @stock[ingredient] += amount 
+    @stock[ingredient] += amount
+  end
+
+  def enough_ingredients_for?(recipe)
+    @stock.all? do |ingredient, amount_in_stock|
+      recipe.ingredients_required.all? do |required_ingredient, required_amount|
+        required_ingredient == ingredient &&
+        amount_in_stock >= required_amount
+      end
+      binding.pry
+    end
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
+require './lib/recipe'
 require './lib/pantry'
+require 'pry'
 
 class PantryTest < Minitest::Test
   def test_it_exists
@@ -27,11 +29,34 @@ class PantryTest < Minitest::Test
     pantry = Pantry.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-    
+
     pantry.restock(ingredient1, 5)
     pantry.restock(ingredient1, 10)
     assert_equal 15, pantry.stock_check(ingredient1)
     pantry.restock(ingredient2, 7)
     assert_equal 7, pantry.stock_check(ingredient2)
+  end
+
+  def test_if_it_has_enough_engredients_for_recipe
+    pantry = Pantry.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    assert_equal false, pantry.enough_ingredients_for?(recipe1)
+    pantry.restock(ingredient2, 7)
+    assert_equal false, pantry.enough_ingredients_for?(recipe1)
+    pantry.restock(ingredient2, 1)
+    assert_equal true, pantry.enough_ingredients_for?(recipe1)
   end
 end
