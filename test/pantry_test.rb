e768b859083877473d813d043b3c65276473df92
Norma Lopez <norma_l304@yahoo.com>
2020-06-03 10:26:22 -0600

    Add pantry class with stock_check method and passing test

---
+++ test/pantry_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_starts_with_no_stock
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_check_stock_of_ingredient
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
+end
